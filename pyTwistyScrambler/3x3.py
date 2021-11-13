67d81de67a5951cc54f05b2034b65a4cea3724ff
mrusse <38119333+mrusse@users.noreply.github.com>
2021-11-13 13:32:59 -0500

    Delete pyTwistyScrambler directory

--- pyTwistyScrambler/3x3.py
+++
@@ -1,7 +0,0 @@
-from pyTwistyScrambler import scrambler335
-import os
-
-with open("/home/pi/CubeTimer/scrambles.txt", "a") as scrambleFile:
-    print("Generating new 3x3 scramble")
-    scrambleFile.write(scrambler333.get_WCA_scramble()+ os.linesep)
-    print("Generation complete")
