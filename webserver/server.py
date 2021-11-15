425e10a4ab3746f120425fbea8fde177cca42226
mrusse <99michaelrussell99@gmail.com>
2021-11-15 12:56:23 -0500

    added 6x6 to webserver and tweaked the style

--- webserver/server.py
+++ webserver/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, send_file, request
 import datetime
-from tabulate import tabulate
 import socket
 import time
 import _thread
@@ -20,27 +19,31 @@ def hello():
 
 @app.route("/solves3x3x3.txt")
 def download3():
-    path = "/home/pi/CubeTimer/solves/solves3x3x3.txt"
+    path = "/home/pi/PiMatTimer/solves/solves3x3x3.txt"
     return send_file(path, as_attachment=True)
 @app.route("/solves2x2x2.txt")
 def download2():
-    path = "/home/pi/CubeTimer/solves/solves2x2x2.txt"
+    path = "/home/pi/PiMatTimer/solves/solves2x2x2.txt"
     return send_file(path, as_attachment=True)
 @app.route("/solves4x4x4.txt")
 def download4():
-    path = "/home/pi/CubeTimer/solves/solves4x4x4.txt"
+    path = "/home/pi/PiMatTimer/solves/solves4x4x4.txt"
     return send_file(path, as_attachment=True)
 @app.route("/solves5x5x5.txt")
 def download5():
-    path = "/home/pi/CubeTimer/solves/solves5x5x5.txt"
+    path = "/home/pi/PiMatTimer/solves/solves5x5x5.txt"
+    return send_file(path, as_attachment=True)
+@app.route("/solves6x6x6.txt")
+def download6():
+    path = "/home/pi/PiMatTimer/solves/solves6x6x6.txt"
     return send_file(path, as_attachment=True)
 @app.route("/solves7x7x7.txt")
 def download7():
-    path = "/home/pi/CubeTimer/solves/solves7x7x7.txt"
+    path = "/home/pi/PiMatTimer/solves/solves7x7x7.txt"
     return send_file(path, as_attachment=True)
 @app.route("/screenshot")
 def screenshot():
-    path = "/home/pi/CubeTimer/screenschot.png"
+    path = "/home/pi/PiMatTimer/screenschot.png"
     os.system("scrot " + path) 
     return send_file(path, as_attachment=True)
 
